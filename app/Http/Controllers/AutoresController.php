44d539dac2b36bdb2258bf0037ee2341fd87c05b
Esmeldy <67498436+Esmeldy@users.noreply.github.com>
2023-10-24 11:32:06 +0200

    Solucionado un pequeño fallo

--- app/Http/Controllers/AutoresController.php
+++ app/Http/Controllers/AutoresController.php
@@ -6,6 +6,7 @@ use App\Models\Autores;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 
+
 class AutoresController extends Controller
 {
     /**
@@ -176,9 +177,9 @@ class AutoresController extends Controller
     }
 
     private function sanitateText(Request $request, $field) {
-        $fieldSanitated = Str::squish($request->$field); //Eliminar espacios
-        $fieldSanitated = mb_strtolower($fieldSanitated, 'UTF-8'); //pasar a minúscula
-        $fieldSanitated = ucfirst($fieldSanitated); //convertir la primera letra en mayúscula
+        $fieldSanitated = Str::of($request->$field)->trim(); //Eliminar espacios
+        $fieldSanitated = Str::lower($fieldSanitated); //pasar a minúscula
+        $fieldSanitated = Str::camel($fieldSanitated); //convertir la primera letra en mayúscula
 
         return $fieldSanitated;
     }
